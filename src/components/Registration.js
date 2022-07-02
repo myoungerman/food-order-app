4f0fc53ac1ca9d6e7c6ef5738343c628aab203db
unknown <mcyoungerman@gmail.com>
2022-07-02 14:58:44 -0700

    Add black background for when button is clicked

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -3,18 +3,40 @@ import './Registration.css';
 import Button from "./Button";
 
 export default function Registration() {
+
+    /*
+    The Create account button hides and disables the existing panel and opens the panel to create an account.
+
+    The login button hides and disables the existing panel and opens the panel to login.
+    */
+
+    let userClickedBtn = false;
+
+    function handleButtonClick(event) {
+        //disable registration panel
+
+        //determine what button was clicked
+    }
+
+    function disableRegistrationPanel() {
+        //add a 
+    }
+
     return (
-        <section class="registration--container">
-            <img src="https://i.postimg.cc/tR55WPV9/order-success.png" className="registration--img"></img>
-            <div className="registration--text-container">
-                <p className="registration--header">Welcome</p>
-                <p className="registration--body">Before enjoying Foodmedia services, please register first.</p>
-            </div>
-            <div className="registration--buttons">
-                <Button className="button--rounded button--dark-green">Create account</Button>
-                <Button className="button--rounded button--light-green">Login</Button>
-            </div>
-            <footer>By logging in or registering, you have agreed to the <a>Terms and Conditions</a> and <a>Privacy Policy</a>.</footer>
-        </section>
+        <div style={{position: "relative"}}>
+            <section className="registration--container">
+                <img src="https://i.postimg.cc/tR55WPV9/order-success.png" className="registration--img"></img>
+                <div className="registration--text-container">
+                    <p className="registration--header">Welcome</p>
+                    <p className="registration--body">Before enjoying Foodmedia services, please register first.</p>
+                </div>
+                <div className="registration--buttons">
+                    <Button className="button--rounded button--dark-green" id="registration--create-account-btn">Create account</Button>
+                    <Button className="button--rounded button--light-green" id="registration--login-btn">Login</Button>
+                </div>
+                <footer>By logging in or registering, you have agreed to the <a>Terms and Conditions</a> and <a>Privacy Policy</a>.</footer>
+            </section>
+            {userClickedBtn && <div className="registration--disable-welcome-panel"></div>}
+        </div>
     )
 }
