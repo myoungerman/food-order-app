4aecc578bd64f5a55c9a838c3d1beef9ddf9f0d4
unknown <mcyoungerman@gmail.com>
2022-07-04 09:54:00 -0700

    Add logic for submitting the registration form

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,6 +3,6 @@ import './Button.css'
 
 export default function Button(props) {
     return (
-        <button id={props.id} className={props.className} type="button" onClick={props.handleClick}>{props.children}</button>
+        <button id={props.id} className={props.className} type={props.type ? props.type : "button"} onClick={props.handleClick}>{props.children}</button>
     )
 }
--- src/components/CreateAccount.js
+++ src/components/CreateAccount.js
@@ -4,26 +4,38 @@ import Button from "./Button";
 
 export default function CreateAccount(props) {
 
-    function submitRegistrationForm() {
-       // validate inputs
+    let inputsAreFilled = props.accountInfo.name && props.accountInfo.email && props.accountInfo.password;
 
-       // make btn green
-       // update state with info
+    function submitRegistrationForm(event) {
+       event.preventDefault();
+       validateInputs(props.accountInfo);
     }
 
-    function validateInputs() {
+    function validateInputs(accountInfo) {
+        if (!accountInfo.email.includes("@")) {
+            return
+         }
+    }
 
+    function handleChange(event) {
+        const {name, value} = event.target;
+
+        // if all inputs are truthy, set button class to green active
+
+        props.setAccountInfo((prevInfo) => {
+            return {...prevInfo, [name]: value};
+        });
     }
 
     return (
-        <form id="createaccount--container">
+        <form id="createaccount--container" onSubmit={submitRegistrationForm}>
             <label>Full Name</label>
-            <input type="text" id="create-acct--full-name" name="fullName" placeholder="Enter your full name" defaultValue={props.accountInfo.name}></input>
+            <input type="text" id="create-acct--full-name" name="name" placeholder="Enter your full name" defaultValue={props.accountInfo.name} onChange={handleChange}></input>
             <label >Email Address</label>
-            <input type="text" id="create-acct--email" name="email" placeholder="Enter your email" defaultValue={props.accountInfo.email}></input>
+            <input type="text" id="create-acct--email" name="email" placeholder="Enter your email" defaultValue={props.accountInfo.email} onChange={handleChange}></input>
             <label>Password</label>
-            <input type="password" id="create-acct--password" name="password" placeholder="**** **** ****" defaultValue={props.accountInfo.password}></input>
-            <Button className="btn--rounded disabled" handleClick={submitRegistrationForm}>Registration</Button>
+            <input type="password" id="create-acct--password" name="password" placeholder="**** **** ****" defaultValue={props.accountInfo.password} onChange={handleChange}></input>
+            <Button className={`btn--rounded ${inputsAreFilled ? "btn--dark-green" : "disabled"}`} type="submit">Register</Button>
             <hr className="line-between-buttons"></hr>
             <Button className="btn--rounded" ><img src="https://i.postimg.cc/RZ3crqSf/icon-google.png"></img>Sign up with Google</Button>
         </form>
