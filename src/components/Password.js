a95a8d746c4c84df24aac30a15cf9430b7a3cb61
unknown <mcyoungerman@gmail.com>
2022-07-11 17:19:15 -0700

    Refactor styling

--- src/components/Password.js
+++ src/components/Password.js
@@ -4,15 +4,15 @@ import Button from "./Button";
 
 export default function Password() {
     return (
-        <div id="pwd--container">
-            <h1>Forgot Password</h1>
-            <p class="light-grey-text">Enter your registered email below.</p>
-            <div id="pwd--email-input-container">
-                <label>Email address</label>
-                <input type="text" id="password--email" name="email" placeholder="Enter your email" defaultValue="" onChange=""></input>
+        <div className="c-pwd">
+            <h1 className="c-pwd__title">Forgot Password</h1>
+            <p className="c-pwd__text">Enter your registered email below.</p>
+            <div className="l-pwd__container">
+                <label className="c-pwd__label">Email address</label>
+                <input className="c-pwd__input" type="text" id="password--email" name="email" placeholder="Enter your email" defaultValue="" onChange=""></input>
             </div>
-            <p class="text--remember-pwd">Remember the password? <a>Sign in</a></p>
-            <Button id="pwd--submit-btn" className="btn--rounded">Submit</Button>
+            <p className="c-pwd__text c-pwd__text--secondary">Remember the password? <a className="c-pwd__link">Sign in</a></p>
+            <Button id="pwd--submit-btn" className="c-pwd__button btn--rounded">Submit</Button>
         </div>
     )
 }
