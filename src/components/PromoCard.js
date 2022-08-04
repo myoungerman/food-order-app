77891be4c8d8a1a06f6298cedf94a2dfbfd7ab4f
unknown <mcyoungerman@gmail.com>
2022-08-04 14:59:24 -0700

    Finish styling PromoCard, style Promos

--- src/components/PromoCard.js
+++ src/components/PromoCard.js
@@ -4,8 +4,8 @@ import './PromoCard.css'
 export default function PromoCard(props) {
     return (
         <div className={`c-promo-card ${props.className ? props.className : ''}`}>
-            <img src={props.companyIcon} className="promo-company-icon c-promo-card__img--icon"></img>
             <div className="l-promo-card__text-container">
+                <img src={props.companyIcon} className="promo-company-icon c-promo-card__img--icon"></img>
                 <h3 className="header--promo c-promo-card__h3">{props.title}</h3>
                 <p className="p--promo-body c-promo-card__text">{props.body}</p>
                 <a className="a--secondary c-promo-card__link">Order<p className="chevron chevron--right c-promo-card__chevron"></p></a>
--- src/components/Promos.js
+++ src/components/Promos.js
@@ -6,15 +6,23 @@ import './Promos.css';
 export default function Promos(props) {
     return (
         <div className={`c-promos ${props.className ? props.className : ''}`}>
-            <PromoCard
-            companyIcon="https://i.postimg.cc/sBmNst6v/burger-king-logo.png"
-            title="Flash Offer"
-            body="Get the best burgers in town for just $1.99 each."
-            cardBackground="https://i.postimg.cc/WthHcYM1/burgers.png"
-            className="background--orange"
-            />
-           {/* <PromoCard></PromoCard>
+            <div className="l-promos__card-container">
+                <PromoCard
+                companyIcon="https://i.postimg.cc/sBmNst6v/burger-king-logo.png"
+                title="Flash Offer"
+                body="Get the best burgers in town for just $1.99 each."
+                cardBackground="https://i.postimg.cc/WthHcYM1/burgers.png"
+                className="c-promos__PromoCard background--orange"
+                />
+                <PromoCard
+                companyIcon="https://i.postimg.cc/Rhk26zc2/dominos-logo.png"
+                title="New Arrivals"
+                body={`Domino's 18" mega meat pizza, just $14.99.`}
+                cardBackground="https://i.postimg.cc/8zcHVvRn/pizza.png"
+                className="c-promos__PromoCard background--green"
+                />
+            </div>
             <PageIndicator />
- */}        </div>
+        </div>
     )
 }
